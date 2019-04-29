b93416547069f4c3d7e3564250be81da5feafb6b
Mark Skinner <markskinner850@gmail.com>
2019-04-29 10:07:35 -0400

    Update style, colors & tweet button

--- js/main.js
+++ js/main.js
@@ -11,9 +11,9 @@ function getRandomQuote() {
       let twitterText = '"' + selection.quote.substring(0, (126 - selection.author.length))
       + '..."' + ' ' + selection.author;
     }
-    $('#quote-container iframe').remove();
-    let tweetDiv = $('<div></div>').addClass('col-12').attr('id', 'tweet-div');
-    let tweetBtn = $('<a></a>').addClass('twitter-share-button')
+    //$('#quote-container iframe').remove();
+    //let tweetDiv = $('<div></div>').addClass('col-6').attr('id', 'tweet-div');
+    /*let tweetBtn = $('<a></a>').addClass('twitter-share-button btn-custom')
       .attr('href', 'http://twitter.com/share')
       .attr('target', '_blank')
       .attr('data-size', 'large')
@@ -21,10 +21,12 @@ function getRandomQuote() {
       .attr('data-hashtags', 'quotes')
       .attr('data-show-count', 'false')
       .attr('data-text', twitterText)
-      .attr('id', 'tweet-btn');
-    $('#quote-container').append(tweetDiv);
-    $('#tweet-div').append(tweetBtn);
-    twttr.widgets.load();
+      .attr('id', 'tweet-btn');*/
+    //$('#button-container').append(tweetDiv);
+    //$('#tweet-div').append(tweetBtn);
+    //twttr.widgets.load();
+
+    $('#tweet-btn').attr('href', 'https://twitter.com/intent/tweet?hashtags=quotes&text=' + encodeURIComponent(twitterText));
   });
 }
 
